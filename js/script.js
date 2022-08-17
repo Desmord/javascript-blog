9900bed0508fef7d5460952904a2f7c2800c8c94
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-08-17 18:51:33 +0200

    Add dynamic tag generating and tag click event handler

--- js/script.js
+++ js/script.js
@@ -30,12 +30,14 @@
 
     const optArticleSelector = '.post',
         optTitleSelector = '.post-title',
-        optTitleListSelector = '.titles';
+        optTitleListSelector = '.titles',
+        optArticleTagsSelector = '.post-tags .list';
 
-    const generateTitleLinks = function () {
+    const generateTitleLinks = function (searchedLinks) {
 
         const titleList = document.querySelector(optTitleListSelector);
         titleList.innerHTML = ``;
+
 
         const articles = document.querySelectorAll(optArticleSelector);
         for (let article of articles) {
@@ -43,10 +45,15 @@
             const articleTitle = article.querySelector(optTitleSelector).innerHTML;
             const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
 
-            // titleList.innerHTML = titleList.innerHTML + linkHTML;
-            titleList.insertAdjacentHTML(`beforeend`, linkHTML);
-        }
+            if (searchedLinks) {
+                if (searchedLinks.length !== 0 && searchedLinks.some(linkIndex => linkIndex == articleId)) {
+                    titleList.insertAdjacentHTML(`beforeend`, linkHTML);
+                }
+            } else {
+                titleList.insertAdjacentHTML(`beforeend`, linkHTML);
+            }
 
+        }
 
         const links = document.querySelectorAll('.titles a');
 
@@ -58,5 +65,71 @@
 
     generateTitleLinks();
 
+    const generateTags = function () {
+        const articles = document.querySelectorAll(optArticleSelector);
+
+
+        for (let article of articles) {
+            const tagsWrapper = article.querySelector(optArticleTagsSelector);
+            const articleTags = article.getAttribute(`data-tags`);
+            const articleTagsArray = articleTags.split(` `);
+            let html = ``;
+
+            for (let tag of articleTagsArray) {
+                html = html + '<li><a href="#tag-' + tag + '">' + tag + '</a></li> ';
+            }
+
+            tagsWrapper.insertAdjacentHTML(`beforeend`, html);
+
+        }
+
+    };
+
+    generateTags();
+
+    const tagClickHandler = function (event) {
+
+        event.preventDefault();
+
+        const clickedEvent = this;
+        const href = getHrefAttribute(clickedEvent.children[0].getAttribute(`href`));
+        const tag = href.substring(4, href.length);
+        const activeLinks = document.querySelectorAll(`a.active`);
+        const allArticles = document.querySelectorAll(`article`);
+        let searchedLinks = [];
+
+        for (let activeLink of activeLinks) {
+            activeLink.classList.remove(`active`);
+        }
+
+        for (let article of allArticles) {
+            const articleTags = article.getAttribute(`data-tags`);
+            const articleTagsArray = articleTags.split(` `);
+            const tagExist = articleTagsArray.some(articleTag => articleTag === tag ? true : false);
+
+            if (tagExist) {
+                const linkIndex = article.getAttribute(`id`);
+                searchedLinks.push(linkIndex);
+            }
+        }
+
+        generateTitleLinks(searchedLinks);
+
+    };
+
+    const addClickListenersToTags = function () {
+        const tagsWrappers = document.querySelectorAll(optArticleTagsSelector);
+
+        for (let tag of tagsWrappers) {
+            const links = tag.querySelectorAll(`li`);
+
+            for (let link of links) {
+                link.addEventListener(`click`, tagClickHandler);
+            }
+        }
+
+    };
+
+    addClickListenersToTags();
 
 }
