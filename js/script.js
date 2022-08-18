4fb13093a92546daa8e4be57bb7c6c65378d9782
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-08-18 13:00:49 +0200

    Add authors link click event

--- js/script.js
+++ js/script.js
@@ -31,9 +31,10 @@
     const optArticleSelector = '.post',
         optTitleSelector = '.post-title',
         optTitleListSelector = '.titles',
-        optArticleTagsSelector = '.post-tags .list';
+        optArticleTagsSelector = '.post-tags .list',
+        optArticleAuthorSelector = `.post-author`;
 
-    const generateTitleLinks = function (searchedLinks) {
+    const generateTitleLinks = function (filtredLinksId) {
 
         const titleList = document.querySelector(optTitleListSelector);
         titleList.innerHTML = ``;
@@ -45,8 +46,8 @@
             const articleTitle = article.querySelector(optTitleSelector).innerHTML;
             const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
 
-            if (searchedLinks) {
-                if (searchedLinks.length !== 0 && searchedLinks.some(linkIndex => linkIndex == articleId)) {
+            if (filtredLinksId) {
+                if (filtredLinksId.length !== 0 && filtredLinksId.some(linkIndex => linkIndex == articleId)) {
                     titleList.insertAdjacentHTML(`beforeend`, linkHTML);
                 }
             } else {
@@ -131,5 +132,64 @@
     };
 
     addClickListenersToTags();
+
+    const generateAuthors = function () {
+        const articles = document.querySelectorAll(optArticleSelector);
+
+
+        for (let article of articles) {
+            const tagsWrapper = article.querySelector(optArticleAuthorSelector);
+            const author = tagsWrapper.getAttribute(`data-author`);
+            let html = '<a href="#author-' + author + '">by ' + author + '</a></li>';
+
+            tagsWrapper.insertAdjacentHTML(`beforeend`, html);
+
+        }
+
+    };
+
+    generateAuthors();
+
+    const authorClickHandler = function (event) {
+        event.preventDefault();
+
+        const clickedEvent = this;
+        const href = getHrefAttribute(clickedEvent.getAttribute(`href`));
+        const author = href.substring(7, href.length);
+        const activeLinks = document.querySelectorAll(`a.active`);
+        const allArticles = document.querySelectorAll(`article`);
+        let filtredLinksIndexs = [];
+
+        for (let activeLink of activeLinks) {
+            activeLink.classList.remove(`active`);
+        }
+
+        for (let article of allArticles) {
+            const articleAuthor = article.querySelector(optArticleAuthorSelector).getAttribute(`data-author`)
+
+            if (author === articleAuthor) {
+                const linkIndex = article.getAttribute(`id`);
+                filtredLinksIndexs.push(linkIndex);
+            }
+
+        }
+
+        generateTitleLinks(filtredLinksIndexs);
+    };
+
+    const addClickListenersToAuthors = function () {
+        const tagsWrappers = document.querySelectorAll(optArticleAuthorSelector);
+
+        for (let tag of tagsWrappers) {
+            const links = tag.querySelectorAll(`a`);
+
+            for (let link of links) {
+                link.addEventListener(`click`, authorClickHandler);
+            }
+        }
+
+    };
+
+    addClickListenersToAuthors();
 
 }
